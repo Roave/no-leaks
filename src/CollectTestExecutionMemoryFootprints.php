a235f18c5b41e30c37415bb79bb3733e6858587c
Marco Pivetta <ocramius@gmail.com>
2019-03-14 22:17:27 +0100

    PHPUnit extension that collects test memory usage and throws on memleaks

---
+++ src/CollectTestExecutionMemoryFootprints.php
@@ -0,0 +1,132 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Roave\NoLeaks\PHPUnit;
+
+use PHPUnit\Framework\AssertionFailedError;
+use PHPUnit\Framework\Test;
+use PHPUnit\Framework\TestListener;
+use PHPUnit\Framework\TestSuite;
+use PHPUnit\Framework\Warning;
+use PHPUnit\Runner\AfterLastTestHook;
+use PHPUnit\Runner\AfterSuccessfulTestHook;
+use PHPUnit\Runner\BeforeTestHook;
+
+/**
+ * Note: we need to implement TestListener, because the hook API is not allowing
+ *       us to interact with test suite instances. This means that the entire package
+ *       may need a rewrite when PHPUnit 9.0 is out, but YOLO.
+ */
+final class CollectTestExecutionMemoryFootprints
+    implements BeforeTestHook,
+    AfterSuccessfulTestHook,
+    AfterLastTestHook,
+    TestListener
+{
+    /** @var array<string, array<int, int>> */
+    private $preTestMemoryUsages = [];
+
+    /** @var array<string, array<int, int>> */
+    private $postTestMemoryUsages = [];
+
+    public function startTestSuite(TestSuite $suite) : void
+    {
+        $suite->addTest(new EmptyBaselineMemoryUsageTest(EmptyBaselineMemoryUsageTest::TEST_METHOD));
+    }
+
+    public function executeBeforeTest(string $test) : void
+    {
+        \gc_collect_cycles();
+
+        $this->preTestMemoryUsages[$test][] = \memory_get_usage();
+    }
+
+    public function executeAfterSuccessfulTest(string $test, float $time) : void
+    {
+        \gc_collect_cycles();
+
+        $this->postTestMemoryUsages[$test][] = \memory_get_usage();
+    }
+
+    public function executeAfterLastTest() : void
+    {
+        if (! (
+            array_key_exists(EmptyBaselineMemoryUsageTest::class . '::' . EmptyBaselineMemoryUsageTest::TEST_METHOD, $this->preTestMemoryUsages)
+            && array_key_exists(EmptyBaselineMemoryUsageTest::class . '::' . EmptyBaselineMemoryUsageTest::TEST_METHOD, $this->postTestMemoryUsages)
+        )) {
+            throw new \Exception('Could not find baseline test: impossible to determine PHPUnit base memory overhead');
+        }
+
+        $baselineMemoryUsage = MeasuredBaselineTestMemoryLeak::fromBaselineTestMemoryUsages(
+            $this->preTestMemoryUsages[EmptyBaselineMemoryUsageTest::class . '::' . EmptyBaselineMemoryUsageTest::TEST_METHOD],
+            $this->postTestMemoryUsages[EmptyBaselineMemoryUsageTest::class . '::' . EmptyBaselineMemoryUsageTest::TEST_METHOD]
+        );
+
+        unset(
+            $this->preTestMemoryUsages[EmptyBaselineMemoryUsageTest::class . '::' . EmptyBaselineMemoryUsageTest::TEST_METHOD],
+            $this->postTestMemoryUsages[EmptyBaselineMemoryUsageTest::class . '::' . EmptyBaselineMemoryUsageTest::TEST_METHOD]
+        );
+
+        $memoryUsages = array_combine(
+            array_keys($this->preTestMemoryUsages),
+            array_map(
+                [MeasuredTestRunMemoryLeak::class, 'fromTestMemoryUsages'],
+                $this->preTestMemoryUsages,
+                $this->postTestMemoryUsages
+            )
+        );
+
+        $leaks = array_filter(array_map(function (MeasuredTestRunMemoryLeak $profile) use ($baselineMemoryUsage) : bool {
+            return $profile->leaksMemory($baselineMemoryUsage);
+        }, $memoryUsages));
+
+        if ([] !== $leaks) {
+            throw new \Exception(sprintf(
+                "The following test produced memory leaks:\n * %s\n",
+                implode("\n * ", array_keys($leaks))
+            ));
+        }
+
+        var_dump($memoryUsages, $leaks, $baselineMemoryUsage);
+    }
+
+    // The following bits are just stubbing the implementation of the
+    // deprecated {@see \PHPUnit\Framework\TestListener\TestListener} API
+
+    public function addError(Test $test, \Throwable $t, float $time) : void
+    {
+    }
+
+    public function addWarning(Test $test, Warning $e, float $time) : void
+    {
+    }
+
+    public function addFailure(Test $test, AssertionFailedError $e, float $time) : void
+    {
+    }
+
+    public function addIncompleteTest(Test $test, \Throwable $t, float $time) : void
+    {
+    }
+
+    public function addRiskyTest(Test $test, \Throwable $t, float $time) : void
+    {
+    }
+
+    public function addSkippedTest(Test $test, \Throwable $t, float $time) : void
+    {
+    }
+
+    public function endTestSuite(TestSuite $suite) : void
+    {
+    }
+
+    public function startTest(Test $test) : void
+    {
+    }
+
+    public function endTest(Test $test, float $time) : void
+    {
+    }
+}
---
+++ test/unit/CollectTestExecutionMemoryFootprintsTest.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RoaveUnitTest\NoLeaks\PHPUnit;
+
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\TestSuite;
+use Roave\NoLeaks\PHPUnit\CollectTestExecutionMemoryFootprints;
+use Roave\NoLeaks\PHPUnit\EmptyBaselineMemoryUsageTest as Baseline;
+
+/** @covers \Roave\NoLeaks\PHPUnit\CollectTestExecutionMemoryFootprints */
+final class CollectTestExecutionMemoryFootprintsTest extends TestCase
+{
+    public function testWillCollectFootprints() : void
+    {
+        $collector = new CollectTestExecutionMemoryFootprints();
+
+        $collector->executeBeforeTest('test1');
+        $mock1 = $this->createMock(\stdClass::class);
+        $collector->executeAfterSuccessfulTest('test1', 0.0);
+        $collector->executeBeforeTest('test1');
+        $mock2 = $this->createMock(\stdClass::class);
+        $collector->executeAfterSuccessfulTest('test1', 0.0);
+        $collector->executeBeforeTest('test1');
+        $mock3 = $this->createMock(\stdClass::class);
+        $collector->executeAfterSuccessfulTest('test1', 0.0);
+
+        // Following section eats double the memory
+        $collector->executeBeforeTest('test2');
+        $mock4 = $this->createMock(\stdClass::class);
+        $mock5 = $this->createMock(\stdClass::class);
+        $collector->executeAfterSuccessfulTest('test2', 0.0);
+        $collector->executeBeforeTest('test2');
+        $mock6 = $this->createMock(\stdClass::class);
+        $mock7 = $this->createMock(\stdClass::class);
+        $collector->executeAfterSuccessfulTest('test2', 0.0);
+        $collector->executeBeforeTest('test2');
+        $mock8 = $this->createMock(\stdClass::class);
+        $mock9 = $this->createMock(\stdClass::class);
+        $collector->executeAfterSuccessfulTest('test2', 0.0);
+
+        $collector->executeBeforeTest(Baseline::class . '::' . Baseline::TEST_METHOD);
+        $mock10 = $this->createMock(\stdClass::class);
+        $collector->executeAfterSuccessfulTest(Baseline::class . '::' . Baseline::TEST_METHOD, 0.0);
+        $collector->executeBeforeTest(Baseline::class . '::' . Baseline::TEST_METHOD);
+        $mock11 = $this->createMock(\stdClass::class);
+        $collector->executeAfterSuccessfulTest(Baseline::class . '::' . Baseline::TEST_METHOD, 0.0);
+        $collector->executeBeforeTest(Baseline::class . '::' . Baseline::TEST_METHOD);
+        $mock12 = $this->createMock(\stdClass::class);
+        $collector->executeAfterSuccessfulTest(Baseline::class . '::' . Baseline::TEST_METHOD, 0.0);
+
+        $this->expectExceptionMessage(<<<'MESSAGE'
+The following test produced memory leaks:
+ * test2
+MESSAGE
+        );
+
+        $collector->executeAfterLastTest();
+
+        $this->consumeMocks($mock1, $mock2, $mock3, $mock4, $mock5, $mock6, $mock7, $mock8, $mock9, $mock10, $mock11, $mock12);
+    }
+
+    /** @return array<int, object> */
+    private function generateSomeMemoryTrash() : array
+    {
+        return array_map(function () : object {
+            return new \stdClass();
+        }, range(1, 100));
+    }
+
+    private function consumeMocks(\stdClass ...$mocks) : void
+    {
+    }
+
+    public function testWillRegisterBaselineTestInTestSuite() : void
+    {
+        $testSuite = $this->createMock(TestSuite::class);
+
+        $testSuite
+            ->expects(self::once())
+            ->method('addTest')
+            ->with(self::equalTo(new Baseline('emptyTest')));
+
+        (new CollectTestExecutionMemoryFootprints())->startTestSuite($testSuite);
+    }
+}
