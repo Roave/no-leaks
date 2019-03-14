05b6a8ed2d82315b505219065d9dce1a5af5edf6
Marco Pivetta <ocramius@gmail.com>
2019-03-14 22:12:15 +0100

    Created an example "leaky" test that loses memory bits here and there

---
+++ test/e2e/LeakyIntegrationTest.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RoaveE2ETest\NoLeaks\PHPUnit;
+
+use PHPUnit\Framework\TestCase;
+
+/** @coversNothing this is an integration test that spans the entirety of the library */
+final class LeakyIntegrationTest extends TestCase
+{
+    /** @var array<int, mixed> */
+    private static $memoryLeakingStupidMistake = [];
+
+    /** @test */
+    public function doesNotLeakMemory() : void
+    {
+        $this->addToAssertionCount(1);
+    }
+
+    /** @test */
+    public function doesLeakATinyAmountOfMemory() : void
+    {
+        self::$memoryLeakingStupidMistake[] = null;
+
+        $this->addToAssertionCount(1);
+    }
+
+    /** @test */
+    public function doesLeakAMock() : void
+    {
+        self::$memoryLeakingStupidMistake[] = $this->createMock(\stdClass::class);
+
+        $this->addToAssertionCount(1);
+    }
+
+    /** @test */
+    public function doesLeakOneObject() : void
+    {
+        self::$memoryLeakingStupidMistake[] = new class {};
+
+        $this->addToAssertionCount(1);
+    }
+
+    /** @test */
+    public function doesLeakTwoObjects() : void
+    {
+        self::$memoryLeakingStupidMistake[] = new class {};
+        self::$memoryLeakingStupidMistake[] = new class {};
+
+        $this->addToAssertionCount(1);
+    }
+
+    /** @test */
+    public function doesLeakTestItself() : void
+    {
+        self::$memoryLeakingStupidMistake[] = $this;
+
+        $this->addToAssertionCount(1);
+    }
+
+    /** @test */
+    public function doesLeakAnAutoloader() : void
+    {
+        spl_autoload_register(function (string $className) : bool {
+            return false;
+        });
+
+        $this->addToAssertionCount(1);
+    }
+
+    /** @test */
+    public function doesLeakAStaticAutoloader() : void
+    {
+        spl_autoload_register(static function (string $className) : bool {
+            return false;
+        });
+
+        $this->addToAssertionCount(1);
+    }
+
+    /** @test */
+    public function doesLeakLotsAndLotsOfMemory() : void
+    {
+        self::$memoryLeakingStupidMistake[] = str_repeat('a', 10000);
+
+        $this->addToAssertionCount(1);
+    }
+}
