d3e20ce38e48e5f668180a444f7f7b2bc5c8f0de
Marco Pivetta <ocramius@gmail.com>
2019-03-14 22:15:09 +0100

    Created a value type for a set of collected test run memory leaks

---
+++ src/MeasuredTestRunMemoryLeak.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Roave\NoLeaks\PHPUnit;
+
+/**
+ * @internal this class is not to be used outside this package
+ *
+ * Instances of this class represent the memory leakage deriving from a single test run.
+ * Such leakages are inevitable, since the test framework needs to track executed test,
+ * statistics, error messages, etc.
+ */
+final class MeasuredTestRunMemoryLeak
+{
+    /** @var array<int, int> positive integers, representing used memory */
+    private $memoryUsages;
+
+    private function __construct(int $firstMemoryUsage, int ...$furtherMemoryUsages)
+    {
+        $this->memoryUsages = array_merge([$firstMemoryUsage], $furtherMemoryUsages);
+        $this->avg = array_sum($this->memoryUsages) / count($this->memoryUsages);
+    }
+
+    /**
+     * @param array<int, int> $preRunMemoryUsages
+     * @param array<int, int> $postRunMemoryUsages
+     */
+    public static function fromTestMemoryUsages(
+        array $preRunMemoryUsages,
+        array $postRunMemoryUsages
+    ) : self {
+        $snapshotsCount = min(count($preRunMemoryUsages), count($postRunMemoryUsages));
+
+        return new self(...array_values(array_map(function (int $beforeRun, int $afterRun) : int {
+            $memoryUsage = $afterRun - $beforeRun;
+
+            if ($memoryUsage < 0) {
+                throw new \Exception(sprintf('Baseline memory usage of %d detected: invalid negative memory usage', $memoryUsage));
+            }
+
+            return $memoryUsage;
+        }, array_slice($preRunMemoryUsages, 0, $snapshotsCount), array_slice($postRunMemoryUsages, 0, $snapshotsCount))));
+    }
+
+    /**
+     * @TODO this implementation is very naïve. We can do more, such as excluding items outside standard deviation,
+     *       or handling of min/max memory usages in collected test results
+     */
+    public function leaksMemory(MeasuredBaselineTestMemoryLeak $baseline) : bool
+    {
+        // If at least one of the runs does not leak memory, then the leak does not come from inside the test,
+        // but from the test runner noise. This is naive, but also an acceptable threshold for most test suites
+        return array_filter(array_map(
+                function (int $memoryUsage) use ($baseline) : bool {
+                    return ! $baseline->lessThan($memoryUsage);
+                },
+                $this->memoryUsages
+            )) === [];
+    }
+}
---
+++ test/unit/MeasuredTestRunMemoryLeakTest.php
@@ -0,0 +1,98 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RoaveUnitTest\NoLeaks\PHPUnit;
+
+use PHPUnit\Framework\TestCase;
+use Roave\NoLeaks\PHPUnit\MeasuredBaselineTestMemoryLeak;
+use Roave\NoLeaks\PHPUnit\MeasuredTestRunMemoryLeak;
+
+/** @covers \Roave\NoLeaks\PHPUnit\MeasuredTestRunMemoryLeak */
+final class MeasuredTestRunMemoryLeakTest extends TestCase
+{
+    public function testMemoryLeakNotDetectedIFAtLeastOneRunIsSameAsBaselineRunLeak() : void
+    {
+        $averageBaselineOf10 = MeasuredBaselineTestMemoryLeak::fromBaselineTestMemoryUsages(
+            [100, 10, 20, 30],
+            [200, 20, 30, 40]
+        );
+
+        $measuredTestLeak = MeasuredTestRunMemoryLeak::fromTestMemoryUsages(
+            [1000, 100, 200, 300],
+            [2000, 110, 220, 330]
+        );
+
+        self::assertFalse($measuredTestLeak->leaksMemory($averageBaselineOf10));
+    }
+
+    public function testMemoryLeakNotDetectedIfAtLeastOneRunIsBelowBaselineRunLeak() : void
+    {
+        $averageBaselineOf10 = MeasuredBaselineTestMemoryLeak::fromBaselineTestMemoryUsages(
+            [100, 10, 20, 30],
+            [200, 20, 30, 40]
+        );
+
+        $measuredTestLeak = MeasuredTestRunMemoryLeak::fromTestMemoryUsages(
+            [1000, 100, 200, 300],
+            [2000, 105, 220, 330]
+        );
+
+        self::assertFalse($measuredTestLeak->leaksMemory($averageBaselineOf10));
+    }
+
+    public function testMemoryLeakDetectedIfAllRunsAreHigherThanBaselineRunLeak() : void
+    {
+        $averageBaselineOf10 = MeasuredBaselineTestMemoryLeak::fromBaselineTestMemoryUsages(
+            [100, 10, 20, 30],
+            [200, 20, 30, 40]
+        );
+
+        $measuredTestLeak = MeasuredTestRunMemoryLeak::fromTestMemoryUsages(
+            [1000, 100, 200, 300],
+            [2000, 111, 220, 330]
+        );
+
+        self::assertTrue($measuredTestLeak->leaksMemory($averageBaselineOf10));
+    }
+
+    public function testRejectsMemoryProfilesWithNegativeLeak() : void
+    {
+        $this->expectExceptionMessage('Baseline memory usage of -1 detected: invalid negative memory usage');
+
+        MeasuredTestRunMemoryLeak::fromTestMemoryUsages(
+            [1000, 100, 200, 300],
+            [2000, 99, 220, 330]
+        );
+    }
+
+    public function testWillOnlyConsiderPreRunMemorySnapshotsForWhichAPostRunSnapshotExists() : void
+    {
+        $averageBaselineOf10 = MeasuredBaselineTestMemoryLeak::fromBaselineTestMemoryUsages(
+            [100, 10, 20, 30],
+            [200, 20, 30, 40]
+        );
+
+        $measuredTestLeak = MeasuredTestRunMemoryLeak::fromTestMemoryUsages(
+            [1000, 200, 300, 400],
+            [2000, 220, 330]
+        );
+
+        self::assertTrue($measuredTestLeak->leaksMemory($averageBaselineOf10));
+    }
+
+    public function testWillOnlyConsiderPostRunMemorySnapshotsForWhichAPreRunSnapshotExists() : void
+    {
+        $averageBaselineOf10 = MeasuredBaselineTestMemoryLeak::fromBaselineTestMemoryUsages(
+            [100, 10, 20, 30],
+            [200, 20, 30, 40]
+        );
+
+        $measuredTestLeak = MeasuredTestRunMemoryLeak::fromTestMemoryUsages(
+            [1000, 200, 300],
+            [2000, 220, 330, 400]
+        );
+
+        self::assertTrue($measuredTestLeak->leaksMemory($averageBaselineOf10));
+    }
+}
