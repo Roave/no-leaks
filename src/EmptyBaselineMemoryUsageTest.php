859b3c4b285c8bc079cfab89a0adbd3e6fa6a246
Marco Pivetta <ocramius@gmail.com>
2019-03-14 22:16:53 +0100

    Created an empty "baseline" test that does nothing
    This test is useful to us for identifyin PHPUnit unavoidable memory usage

---
+++ src/EmptyBaselineMemoryUsageTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Roave\NoLeaks\PHPUnit;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal this class is not to be used outside this package
+ *
+ * @coversNothing
+ */
+final class EmptyBaselineMemoryUsageTest extends TestCase
+{
+    public const TEST_METHOD = 'emptyTest';
+
+    /**
+     * An empty baseline test that will be used to measure pre- and post-run
+     * memory usage, to be compared with the rest of the test suite runs.
+     *
+     * Tests that deviate from this baseline pre/post memory usage are most
+     * likely leaking.
+     */
+    public function emptyTest() : void
+    {
+        $this->addToAssertionCount(1);
+    }
+}
---
+++ test/unit/EmptyBaselineMemoryUsageTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RoaveUnitTest\NoLeaks\PHPUnit;
+
+use PHPUnit\Framework\TestCase;
+use Roave\NoLeaks\PHPUnit\EmptyBaselineMemoryUsageTest as Baseline;
+
+/** @covers \Roave\NoLeaks\PHPUnit\EmptyBaselineMemoryUsageTest */
+final class EmptyBaselineMemoryUsageTest extends TestCase
+{
+    public function testDoesNotProduceMemoryLeaks() : void
+    {
+        $test = new Baseline();
+
+        $memoryUsage = 1;
+
+        // intentional immediate value override - we're offsetting memory usage here, on purpose
+        $memoryUsage = memory_get_usage();
+
+        $test->emptyTest();
+
+        self::assertSame($memoryUsage, memory_get_usage());
+    }
+}
