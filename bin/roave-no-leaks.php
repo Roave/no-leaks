8c1076671461cc322803be8067d69e680e88e413
Marco Pivetta <ocramius@gmail.com>
2019-03-15 20:15:21 +0100

    Allow usage of CLI when the package is located in vendor
    Ref: https://github.com/Roave/no-leaks/pull/1#discussion_r265937348

--- bin/roave-no-leaks.php
+++ bin/roave-no-leaks.php
@@ -7,7 +7,11 @@ namespace Roave\NoLeaks\CLI;
 use PHPUnit\TextUI\Command;
 
 (function () {
-    require_once __DIR__ . '/../vendor/autoload.php';
+    if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+        require_once __DIR__ . '/../vendor/autoload.php';
+    } else {
+        require_once __DIR__ . '/../../../vendor/autoload.php';
+    }
 
     $_SERVER['argv'][] = '--repeat=3';
 
